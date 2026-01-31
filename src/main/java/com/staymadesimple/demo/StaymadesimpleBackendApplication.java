248c59d1a66eb49bdfcd355710407b9463dac063
Vaibhav Sahni <sahnidlf@gmail.com>
2026-01-31 09:36:25 +0530

    added spring initializer
    Springboot files
    
    Dependencies:
    Spring Web
    Spring Data JPA
    PostgreSQL Driver
    Flyway

---
+++ src/main/java/com/staymadesimple/demo/StaymadesimpleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.staymadesimple.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaymadesimpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaymadesimpleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staymadesimple/demo/StaymadesimpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staymadesimple.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaymadesimpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
